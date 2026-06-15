7a5641fe4f1bed929d724456eb21eb5a398fb7ed
srujan4705 <22951a67b5@iare.ac.in>
2026-06-15 16:27:41 +0530

    cors changed

--- backend/server.js
+++ backend/server.js
@@ -143,23 +143,12 @@ const COLUMN_ALIASES = {
   created_date: "created_at",
 };
 
-const corsOptions = {
-  origin: [
-    "http://localhost:5173",
-    "https://learnorbit.online",
-    "https://www.learnorbit.online",
-  ],
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-};
-
-app.use(cors(corsOptions));
-
-app.use((req, res, next) => {
-  console.log(req.method, req.url);
-  next();
-});
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
